1208bac33b5926b9b7e8f7870386fb025aec1248
Bryan Lopez <bryanlopezrr@gmail.com>
2020-03-05 22:33:39 -0500

    This branch can succesfully sign in to an account and create one

--- app/src/main/java/com/example/onsen/LoginActivity.java
+++ app/src/main/java/com/example/onsen/LoginActivity.java
@@ -2,16 +2,63 @@ package com.example.onsen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText tEmail, tPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        tEmail = findViewById(R.id.editTextEmail);
+        tPassword = findViewById(R.id.editTextPassword);
 
     }
+
+    public void login(View view){
+
+        if( TextUtils.isEmpty(tPassword.getText()) &&  TextUtils.isEmpty(tEmail.getText())) {
+            tEmail.setError( "Email is required" );
+            tPassword.setError("Password is required");
+        }
+        else if( TextUtils.isEmpty(tEmail.getText())){
+            tEmail.setError( "Email is required" );
+
+        }
+        else if( TextUtils.isEmpty(tPassword.getText())) {
+            tPassword.setError("Password is required");
+        }
+        else{
+            ParseUser.logInInBackground(tEmail.getText().toString(), tPassword.getText().toString(), new LogInCallback() {
+                @Override
+                public void done(ParseUser parseUser, ParseException e) {
+                    if (parseUser != null) {
+                        Toast.makeText(LoginActivity.this, "Sucessful Login - Welcome back" + tEmail.getText().toString() + "!", Toast.LENGTH_LONG).show();
+                    } else {
+                        ParseUser.logOut();
+                        Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+
+    }
+
+    public void signUp(View view){
+        Intent intent = new Intent( LoginActivity.this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/onsen/SignUpActivity.java
@@ -0,0 +1,75 @@
+package com.example.onsen;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    EditText tName, tEmail, tPassword, tConfirmPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        tName = findViewById(R.id.editTextName);
+        tEmail = findViewById(R.id.editTextEmail);
+        tPassword = findViewById(R.id.editTextPassword);
+        tConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+
+    }
+
+    public void signUp(View view){
+
+        if( TextUtils.isEmpty(tName.getText())){
+            tName.setError( "Name is required" );
+
+        }
+        else if( TextUtils.isEmpty(tEmail.getText())) {
+            tEmail.setError("Email is required");
+        }
+        else if( TextUtils.isEmpty(tPassword.getText())) {
+            tPassword.setError("Password is required");
+        }
+        else if( TextUtils.isEmpty(tConfirmPassword.getText())) {
+            tConfirmPassword.setError("Confirmed Password is required");
+        }
+        else if(!tConfirmPassword.getText().toString().equals(tConfirmPassword.getText().toString())) {
+            Toast.makeText(SignUpActivity.this, "Password's dont match", Toast.LENGTH_LONG).show();
+        }
+        else{
+            ParseUser user = new ParseUser();
+            // Set the user's username and password, which can be obtained by a forms
+            user.setUsername(tEmail.getText().toString().trim());
+            user.setEmail(tEmail.getText().toString().trim());
+            user.setPassword(tPassword.getText().toString().trim());
+
+            user.put("name", tName.getText().toString().trim());
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        Toast.makeText(SignUpActivity.this, "Welcome!", Toast.LENGTH_LONG).show();
+                    } else {
+                        ParseUser.logOut();
+                        Toast.makeText(SignUpActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+
+        }
+
+    }
+
+
+}
