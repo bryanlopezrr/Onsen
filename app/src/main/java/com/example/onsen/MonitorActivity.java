d5688411a8390e65f950d1dd556ec0ee6717e371
Bryan Lopez <bryanlopezrr@gmail.com>
2020-03-06 23:01:34 -0500

    added nav bar to one activity

--- app/src/main/java/com/example/onsen/MonitorActivity.java
+++ app/src/main/java/com/example/onsen/MonitorActivity.java
@@ -1,6 +1,8 @@
 package com.example.onsen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,25 @@ public class MonitorActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitor);
+    }
+
+    public void goToReports(View view){
+        Intent intent = new Intent( MonitorActivity.this, ReportsActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToMonitor(View view){
+        Intent intent = new Intent( MonitorActivity.this, MonitorActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToMeditate(View view){
+        Intent intent = new Intent( MonitorActivity.this, MeditateActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToReminder(View view){
+        Intent intent = new Intent( MonitorActivity.this, ReminderActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/onsen/ReportsActivity.java
+++ app/src/main/java/com/example/onsen/ReportsActivity.java
@@ -1,6 +1,8 @@
 package com.example.onsen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,4 +15,28 @@ public class ReportsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_reports);
 
     }
+
+    public void goToReports(View view){
+        Intent intent = new Intent( ReportsActivity.this, ReportsActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToMonitor(View view){
+        Intent intent = new Intent( ReportsActivity.this, MonitorActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToMeditate(View view){
+        Intent intent = new Intent( ReportsActivity.this, MeditateActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToReminder(View view){
+        Intent intent = new Intent( ReportsActivity.this, ReminderActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
 }
